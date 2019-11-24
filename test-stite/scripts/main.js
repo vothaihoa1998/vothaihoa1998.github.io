cb2a3108c4145bdb11ac965109124f8d30892ddc
vothaihoa1998 <thaihoavo1998@gmail.com>
2019-11-24 16:29:17 +0700

    Add files via upload

---
+++ test-stite/scripts/main.js
@@ -0,0 +1,3 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent="Hello word";
+// JavaScript phân biệt hoa thường — myVariable và myvariable là hai biến khác nhau. Nếu có vấn đề trong mã nguồn, hãy kiểm tra chữ hoa-thường!
